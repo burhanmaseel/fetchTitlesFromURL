3f7d2f914372c1eba388db21d8c7a01b901d728d
Burhan Maseel <burhanmaseel@users.noreply.github.com>
2019-05-20 23:37:07 +0500

    Request Response Generated

--- controllers/titleControllers.js
+++ controllers/titleControllers.js
@@ -1,6 +1,24 @@
+var async = require("async");
+const { extractTitle } = require("../helpers/getTitleHelpers");
+const request = require("request");
 
-
-exports.getTitles = (req, res) => {
-    console.log(typeof (req.query.address));
-}
-
+exports.getTitles = async (req, res) => {
+  let address = req.query.address;
+  if (!Array.isArray(req.query.address)) {
+    address = [req.query.address];
+  }
+  let titles = await Promise.all(
+    address.map(url => {
+      return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+          let title = body
+            .split("<title>")
+            .pop()
+            .split("</title>")[0];
+          resolve(title);
+        });
+      });
+    })
+  );
+  console.log(titles);
+};
